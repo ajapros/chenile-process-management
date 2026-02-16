c843d0e2406027eba964229d91236d76b806b16f
Gaurav Bhardwaj <gauravbhardwaj@VYMO-IT-LPT-730.local>
2026-02-16 13:37:36 +0530

    Fix compile issue

--- process-service/src/test/java/org/chenile/orchestrator/process/bdd/CukesRestTest.java
+++ process-service/src/test/java/org/chenile/orchestrator/process/bdd/CukesRestTest.java
@@ -1,7 +1,7 @@
 package org.chenile.orchestrator.process.bdd;
 
-import cucumber.api.CucumberOptions;
-import cucumber.api.junit.Cucumber;
+import io.cucumber.junit.Cucumber;
+import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ActiveProfiles;
 
